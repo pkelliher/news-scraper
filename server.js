e9f136c998690e879e6e93137a27d6b80ef3e595
unknown <pkell@DESKTOP-ED2GH22.(none)>
2019-08-30 23:05:17 -0700

    Heroku app created and mLab connection established

--- server.js
+++ server.js
@@ -39,4 +39,9 @@ mongoose.connect(MONGODB_URI);
 // Listen on the port
 app.listen(PORT, () => {
   console.log("Listening on port: " + PORT);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 });
